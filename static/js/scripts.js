167d7f4082231ac3fa0cda3c05e1bbe03ee0e75b
JosueAVD27 <ajvelasquez4@espe.edu.ec>
2022-07-20 00:03:59 -0500

    Version 6 - Final

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,32 +1,45 @@
+//Formato de correo electronico
 var expr = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
 
+//Crea la funcion
 $(document).ready(function(){
     $("#fEnviar").click(function(){
+        //crea Variables y obtiene los campos del formulario de validaciones 
         var nombre = $("#fNombre").val();
         var apellido = $("#fApellido").val();
         var edad = $("#fedad").val();
         var email = $("#femail").val();
 
+        //Validacion de nombre
         if (nombre == ""){
-            $("#mensaje1").fadeIn();
+            $("#mensaje1").fadeIn();        //Muestra el campo de validacion
             return false;
         }else{
-            $("#mensaje1").fadeOut();
+            $("#mensaje1").fadeOut();        //Oculta el campo de validacion
+            //Validacion de apellido
             if(apellido == ""){
-                $("#mensaje2").fadeIn();
+                $("#mensaje2").fadeIn();        //Muestra el campo de validacion
                 return false;
             }else{
-                $("#mensaje2").fadeOut();
-                if(edad == ""){
-                    $("#mensaje3").fadeIn();
+                $("#mensaje2").fadeOut();        //Oculta el campo de validacion
+                //Validacion de edad
+                if(edad == "" ){
+                    $("#mensaje3").fadeIn();        //Muestra el campo de validacion
                     return false;
                 }else{
-                    $("#mensaje3").fadeOut();
+                    //Validacion cuando la edad esta fuera del rango de 0 a 100
+                    if(edad > 0 && edad < 100){
+                        $("#mensaje3").fadeOut();        //Oculta el campo de validacion
+                    }else{
+                        $("#mensaje3").fadeIn();        //Muestra el campo de validacion
+                        return false;
+                    }
+                    //Validacion de correo
                     if(email == "" || !expr.test(email)){
-                        $("#mensaje4").fadeIn();
+                        $("#mensaje4").fadeIn();        //Muestra el campo de validacion
                         return false;
                     }else{
-                        $("#mensaje4").fadeOut();
+                        $("#mensaje4").fadeOut();        //Oculta el campo de validacion
                     }
                 }
             }
